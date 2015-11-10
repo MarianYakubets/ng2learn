9cdea2ff06ed6c3e52645c5ae1512431e8925dfd
MarianYakubets <marian.yakubets@gmail.com>
2015-11-10 21:50:42 +0200

    First commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import {Component, bootstrap} from 'node_modules/angular2/angular2';
+import {Component, bootstrap} from 'angular2/angular2';
 @Component({
     selector: 'my-app',
     template: '<h1>My First Angular 2 App at MAC</h1>'
